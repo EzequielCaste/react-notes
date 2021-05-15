22df3dedd1ff68924161de860d1c2645e924596f
EzequielCaste <ezequiel.caste@gmail.com>
2021-05-15 13:35:44 -0300

    Base app and styles

---
+++ src/NotesApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const NotesApp = () => {
+  return (
+    <div className="container">
+      <h1>notes app</h1>
+      <div className="btn-new-note">New Note</div>
+    </div>
+  );
+};
