434587d1d0fd55aadd1251035cdfde00593a17ea
EzequielCaste <ezequiel.caste@gmail.com>
2021-05-15 19:26:05 -0300

    feat: create NoteCard component and mock notes

--- src/NotesApp.js
+++ src/NotesApp.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import NoteCard from './NoteCard';
 
 export const NotesApp = () => {
+  const notes = [
+    {title: 'note 1', id: 1, content: 'note 1'},
+    {title: 'note 2', id: 2, content: 'note 2'},
+    {title: 'note 3', id: 3, content: 'note 3'},
+  ];
+  const notesComponent = notes.map((note) => (
+    <NoteCard note={note} key={note.id} />
+  ));
   return (
     <div className="container">
       <h1>notes app</h1>
+      <div className="Notes-container">{notesComponent}</div>
       <div className="btn-new-note">New Note</div>
     </div>
   );
